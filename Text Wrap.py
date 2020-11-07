af67bd80ce2f08fe33a72b5a3f351e6e43b21a21
Sorabh <sorabhsingh4@gmail.com>
2020-11-07 20:28:25 +0530

    ok

--- Text Wrap.py
+++ Text Wrap.py
@@ -2,5 +2,6 @@ def wrap(string, max_width):
     store =[]
     for i in range(0,len(string),max_width):
         a = string[i:i+max_width]
-        store.append(a) 
+        store.append(a)
+        # logic
     return '\n'.join(store) 
