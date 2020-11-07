c8984a477634ed422b4b6eeaefc5dfd3f1e0c0c5
Sorabh <sorabhsingh4@gmail.com>
2020-11-07 08:15:12 +0530

    working ok

---
+++ lists.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    N = int(input())
+    lis = []
+
+    for _ in range(N):
+        command, *value = input().split()
+
+        if command == "print":
+            print(lis)
+
+        else:
+            getattr(lis, command)(*(map(int, value)))
