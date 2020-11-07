3fb9759f706f3141e25f7e38c37df950d094fba5
Sorabh <sorabhsingh4@gmail.com>
2020-11-07 20:46:14 +0530

    explanation added

--- itertools.product.py
+++ itertools.product.py
@@ -2,9 +2,8 @@ from itertools import product
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 re = list(product(a, b))
+print(*re)
 
-result = ''
-for i in re:
-    result += str(i) + ' '
-print(result)
-
+# Explanation:-
+# itertools.product() is equal to nested loop. it is an inbuilt function and works faster than nested loop
+# It gives output in iterable object so we used list to covert it into list and then we used * to unpack the list
