c910e8989c4e61021451a30616a3d79068832dd7
Sorabh <sorabhsingh4@gmail.com>
2020-11-07 19:35:36 +0530

    tricky problem, somehow managed to fix

---
+++ String Formatting.py
@@ -0,0 +1,20 @@
+def print_formatted(number):
+    # your code goes hereclear
+    results = []
+
+    for i in range(1, n+1):
+        decimal = str(i)
+        octal = str(oct(i)[2:])
+        hex_ = str(hex(i)[2:]).upper()
+        binary = str(bin(i)[2:])
+
+        results.append([decimal, octal, hex_, binary])
+
+    width = len(results[-1][3])  # largest binary number
+
+    for i in results:
+        print(*(rep.rjust(width) for rep in i))
+        
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
