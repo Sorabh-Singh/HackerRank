685012bb56b0d48c2ecffa0c98299633e5a5eafd
Sorabh <sorabhsingh4@gmail.com>
2020-11-09 17:55:45 +0530

    tertool combinations concept

---
+++ itertools.combinations().py
@@ -0,0 +1,7 @@
+from itertools import combinations
+my_str, rep_size = input().split()
+
+for num in range(1, int(rep_size)+1):
+    possible_combs = combinations(sorted(my_str), num)
+    for i in possible_combs:
+        print(''.join(i))
