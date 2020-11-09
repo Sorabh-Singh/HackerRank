76513b98311dde3210ab230316b7de19bbc302fd
Sorabh <sorabhsingh4@gmail.com>
2020-11-09 20:50:16 +0530

    added union concept

--- Set .difference() Operation.py
+++ Set .difference() Operation.py
@@ -4,3 +4,13 @@ e = set(map(int, input().split()))
 french_num = int(input())      
 f = set(map(int, input().split()))
 print(len(e-f))
+
+# Set .union() Operation
+
+eng_num = int(input())
+e = set(map(int, input().split()))
+
+french_num = int(input())      
+f = set(map(int, input().split()))
+print(len(e.union(f)))
+
