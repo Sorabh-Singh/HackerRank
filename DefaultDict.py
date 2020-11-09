be673f51892b951c3131a48574b69775f827b58f
Sorabh <sorabhsingh4@gmail.com>
2020-11-09 17:15:37 +0530

    DefaultDict concept, good conecpt to remember

--- DefaultDict.py
+++ DefaultDict.py
@@ -1,4 +1,4 @@
-
+# Approach:1
 a , b = map(int, input().split())
 words_a=[input() for _ in range(a)]
 words_b=[input() for _ in range(b)]
@@ -10,9 +10,42 @@ for i in words_b:
         elif i not in words_a:
             print('-1')
     print()
-        # if i not in words_a :
-        #     print("-1")
-    
+
+# Approach:2
+from collections import defaultdict
+
+# Inputs
+# ------
+n, m = map(int, input().split(' '))
+
+# Let's get the groups as lists
+# -----------------------------
+#input1 = ['a', 'a', 'b', 'a', 'b']
+#input2 = ['a', 'b']
+input1 = list()
+for i in range(n):
+    input1.append(input())
+
+    input2 = list()
+for i in range(m):
+    input2.append(input())
+
+# Calculate Output
+# ----------------
+d = defaultdict(list)
+
+# Fill d with input1 values
+for i in range(n):
+    d[input1[i]].append(i+1)
+#print(d) --> defaultdict(<class 'list'>, {'a': [1, 2, 4], 'b': [3, 5]})
+
+# Apply the logic for printing
+for i in input2:    
+    if i in d:
+        print(*d[i])
+    else:
+        print(-1)
+
         
 
 
