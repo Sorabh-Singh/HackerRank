dfd7188dc00e9abcbe2600640c216519359f0df7
Sorabh <sorabhsingh4@gmail.com>
2020-12-09 09:49:01 +0530

    evalution concept

---
+++ eval().py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/input/problem
+
+x,k = list(map(int, input().split()))
+eq = input()
+
+print(eval(eq)==k)
+
+
+# ui = input().split()
+# x = int(ui[0])
+# print(eval(input()) == int(ui[1]))
