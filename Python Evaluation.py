0d53df43cc0043caa9241142bd39d95711ba457f
Sorabh <sorabhsingh4@gmail.com>
2020-12-10 22:40:27 +0530

    running as expected

---
+++ Python Evaluation.py
@@ -0,0 +1,12 @@
+'''
+The eval() expression is a very powerful built-in function of Python. It helps in evaluating an expression. The expression can be a Python statement, or a code object.
+For example:
+>>> eval("9 + 5")
+14
+>>> x = 2
+>>> eval("x + 3")
+5
+'''
+# solution
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+eval(input())
