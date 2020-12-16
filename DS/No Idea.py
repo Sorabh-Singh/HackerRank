8dbf6259a1598c64109ca0159a3ac9960cfae83b
Sorabh <sorabhsingh4@gmail.com>
2020-12-16 23:30:43 +0530

    sparse array, goot concept to remember

--- DS/No Idea.py
+++ DS/No Idea.py
@@ -23,20 +23,16 @@ n,m = map(int,input().split())
 N = list(map(int,input().split()))
 A = set(map(int,input().split()))
 B = set(map(int,input().split()))
-print(A, B)
 
 happiness_level= 0
 
 for i in N:
     if i in A:
-        print("A before", i, happiness_level)
         happiness_level = happiness_level + 1
-        print("A after", i, happiness_level)
     
     elif i in B:
-        print("B before", i, happiness_level)
         happiness_level = happiness_level - 1
-        print("B after ", i, happiness_level)
+
 print(happiness_level)
 
 
