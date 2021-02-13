2f6ceb3edfdc736e55678f49acc712d11c28ee4d
Sorabh <sorabhsingh4@gmail.com>
2021-02-13 13:05:02 +0530

    Added some concepts

--- Python Projects/BMI index calculator.py
+++ Python Projects/BMI index calculator.py
@@ -22,24 +22,22 @@ def bmi_calculator(height, weight):
     bmi = W/(H*H)
     return bmi
 
+
 height = float(input("your height in cms : "))
 weight = float(input("your weight in kgs : "))
 
 BMI = bmi_calculator(height, weight)
 
 
-if(BMI>0):
-	if(BMI<=16):
-		print("you are severely underweight")
-	elif(BMI<=18.5):
-		print("you are underweight")
-	elif(BMI<=25):
-		print("you are Healthy")
-	elif(BMI<=30):
-		print("you are overweight")
-	else: print("you are severely overweight")
-print("Calculated BMI is : ", BMI)
-
-
-
-
+if(BMI > 0):
+    if(BMI <= 16):
+        print("you are severely underweight")
+    elif(BMI <= 18.5):
+        print("you are underweight")
+    elif(BMI <= 25):
+        print("you are Healthy")
+    elif(BMI <= 30):
+        print("you are overweight")
+    else:
+        print("you are severely overweight")
+print("Calculated BMI Index is : ", BMI)
