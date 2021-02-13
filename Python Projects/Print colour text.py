16301105a8f0aab9f1199241ca0d95076888bd2c
Sorabh <sorabhsingh4@gmail.com>
2021-02-13 12:28:44 +0530

    fixed naming convention.

--- Python Projects/Print colour text.py
+++ Python Projects/Print colour text.py
@@ -2,6 +2,6 @@ import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
-print(Fore.BLUE+Back.YELLOW+"Hi My name is Aman Kharwal "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
-print(Back.CYAN+"Hi My name is Aman Kharwal")
-print(Fore.RED + Back.GREEN+ "Hi My name is Aman Kharwal")
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Sorabh Singh "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
+print(Back.CYAN+"Hi My name is Sorabh Singh")
+print(Fore.RED + Back.GREEN+ "Hi My name is Sorabh Singh")
