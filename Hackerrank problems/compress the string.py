2a419f6fd1deee48b990c6ebda97fbfba50e0175
Sorabh <sorabhsingh4@gmail.com>
2021-02-14 23:45:04 +0530

    group by concept , important concept to remember

---
+++ Hackerrank problems/compress the string.py
@@ -0,0 +1,7 @@
+from itertools import groupby
+
+for key, group in groupby(input("Enter string here : ")): 
+    print(key, group)
+    key= int(key)
+    count = len(list(group))
+    print((count,key),end=" ")
