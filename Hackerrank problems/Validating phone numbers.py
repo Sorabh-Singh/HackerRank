29010e64add57343be5357d072dc793687f1c53c
Sorabh <sorabhsingh4@gmail.com>
2021-02-28 09:53:57 +0530

    array total problem

--- Hackerrank problems/Validating phone numbers.py
+++ Hackerrank problems/Validating phone numbers.py
@@ -1,14 +1,21 @@
-# import re
-# for _ in range(int(input())):
-#     if re.match(r'[789]\d{9}$',input()):   
-#         print('YES')  
-#     else:  
-#         print('NO')
-count = int(input())
-for _ in range(count):
-    raw_number = input()
-    if len(raw_number)==10 and raw_number[0] in ['7', '8', '9'] and raw_number.isnumeric():
-        print("YES")
-    else:
-        print("NO")
+# Approach:1
+'''
+9587456281
+1252478965
+'''
+import re
+for _ in range(int(input())):
+    if re.match(r'[789]\d{9}$',input()):   
+        print('YES')  
+    else:  
+        print('NO')
+
+# Approach:2
+# count = int(input())
+# for _ in range(count):
+#     raw_number = input()
+#     if len(raw_number)==10 and raw_number[0] in ['7', '8', '9'] and raw_number.isnumeric():
+#         print("YES")
+#     else:
+#         print("NO")
 
---
+++ Hackerrank problems/array_total.py
@@ -0,0 +1,23 @@
+# Python program to find if there are
+# two elements wtih given sum
+
+# function to check for the given sum
+# in the array
+def printPairs(arr, arr_size, sum):
+	
+	# Create an empty hash set
+	s = set()
+	
+	for i in range(0, arr_size):
+		temp = sum-arr[i]
+		if (temp in s):
+			print("Pair with given sum "+ str(sum) +
+	" is (" + str(arr[i]) + ", " + str(temp) + ")")
+		s.add(arr[i])
+
+# driver code
+A = [1, 4, 45, 6, 10, 8]
+n = 16
+printPairs(A, len(A), n)
+
+# This code is contributed by __Devesh Agrawal__
